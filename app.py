f72ac8fcb0eb7fcbb201d1e81130170d97e27a67
supreim <piccardharris@gmail.com>
2025-01-30 18:13:38 -0500

    Implemented the parallel function

--- app.py
+++ app.py
@@ -1,6 +1,13 @@
 def parallel(resistances):
-    pass
+    total = 0;
+    for resistance in resistances:
+        total += 1/resistance
+    total = round(1/total, 3)
+    print(f"{total} ohms")
+
 def potential_divider(volt, resistances):
     pass
 def temperature_check(temp,unit):
     pass
+
+parallel([330,1000,2200])
